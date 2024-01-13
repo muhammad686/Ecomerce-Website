f0a4c018340dc176cc0254f3aa776d2c053c0ae0
muhammad686 <tahamuhammadi870@gmail.com>
2024-01-13 07:24:41 +0500

    cart component added

--- ECW/client/src/App.js
+++ ECW/client/src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/common/header";
 import Footer from "./components/common/footer";
-import Home from "./pages/Home";
-import Account from "./pages/Account";
+// import Home from "./pages/Home";
+// import Account from "./pages/Account";
 import Cart from "./components/cart/cart";
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      {/* <Home></Home> */}
       {/* <Account></Account> */}
       <Cart></Cart>
       <Footer></Footer>
--- ECW/client/src/components/cart/cart.jsx
+++ ECW/client/src/components/cart/cart.jsx
@@ -3,19 +3,69 @@ import "../../assets/styles/cart.css";
 const Cart = () => {
   return (
     <div className="main-card-container">
-      <div className="cards-container">
-        <div className="img"></div>
-        <div className="details">
-          <p>product name</p>
-          <p>product rating</p>
+      <div className="cart-list">
+        <div className="cards-container">
+          <div className="img"></div>
+          <div className="cart-item-description">
+            <div className="item-name-and-price">
+              <h4>Quad Camera</h4>
+              <h4>$99.99</h4>
+            </div>
+            <div className="item-rating-and-cart">
+              <p>5 Colors available</p>
+              <div class="rating">
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span class="rating-text">(121)</span>
+              </div>
+            </div>
+          </div>
+          <div className="action-links">
+            <a href="#">Remove</a>
+            <a href="#"> Move to Wishlist</a>
+          </div>
+          <div className="price"></div>
         </div>
-        <div className="action-links"></div>
-        <div className="price"></div>
+        <div className="checkout-container">
+          <h6>Total:</h6>
+          <h2>$199</h2>
+          <button type="submit">Checkout</button>
+        </div>
       </div>
-      <div className="checkout-container">
-        <h4>Total</h4>
-        <h2>$199</h2>
-        <button type="submit">Checkout</button>
+      <div className="cart-list">
+        <div className="cards-container">
+          <div className="img"></div>
+          <div className="cart-item-description">
+            <div className="item-name-and-price">
+              <h4>Quad Camera</h4>
+              <h4>$99.99</h4>
+            </div>
+            <div className="item-rating-and-cart">
+              <p>5 Colors available</p>
+              <div class="rating">
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span class="rating-text">(121)</span>
+              </div>
+            </div>
+          </div>
+          <div className="action-links">
+            <a href="#">Remove</a>
+            <a href="#"> Move to Wishlist</a>
+          </div>
+          <div className="price"></div>
+        </div>
+        <div className="checkout-container">
+          <h6>Total:</h6>
+          <h2>$199</h2>
+          <button type="submit">Checkout</button>
+        </div>
       </div>
     </div>
   );
